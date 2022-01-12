3853b922fd7a49e133977c60d81d1e20d94737d7
Moez <65367982+moezmustafa@users.noreply.github.com>
2022-01-12 08:10:08 +0500

    union , struct , enum

---
+++ structures/harry.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+
+using namespace std ; 
+
+typedef struct user { 
+    string name ; 
+    int age ; 
+    string address ; 
+    string phone ; 
+    string email ; 
+    string password ; 
+}us;
+
+int main()
+{
+
+  struct user harry ;
+    harry.name = "Harry" ;
+    harry.age = 20 ;
+    harry.address = "Kathmandu" ;
+    harry.phone = "9841234567" ;
+    harry.email = "kuta@something.com";
+    harry.password = "12345" ;
+
+
+    us moeez ; 
+    moeez.name = "Moeez" ;
+    moeez.age = 20 ;
+    moeez.address = "Kathmandu" ;
+    moeez.phone = "9841234567" ;
+    moeez.email = "afadsf";
+    moeez.password = "12345" ;
+
+
+
+
+
+    cout << "Name : " << harry.name << endl ;
+    cout << "Age : " << harry.age << endl ;
+    cout << "Address : " << harry.address << endl ;
+    cout << "Phone : " << harry.phone << endl ;
+    cout << "Email : " << harry.email << endl ;
+    cout << "Password : " << harry.password << endl ;
+
+cout << endl;
+    // print us moeez 
+    cout << "Name : " << moeez.name << endl ;
+    cout << "Age : " << moeez.age << endl ;
+    cout << "Address : " << moeez.address << endl ;
+    cout << "Phone : " << moeez.phone << endl ;
+    cout << "Email : " << moeez.email << endl ;
+    cout << "Password : " << moeez.password << endl ;
+
+
+
+    return 0 ;
+
+
+}
