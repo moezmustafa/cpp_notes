916d7199fc9d2aa2d2d7b070b34dd15f9437ae75
Moez <65367982+moezmustafa@users.noreply.github.com>
2022-04-01 18:28:30 +0500

    used the revert command to get back the oop folder

---
+++ Object Oriented Programming/Classes and objects/classes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+
+
+//what are classes ? they are the user defined data types or stencils that can be used to create objects
+//what are objects ? they are the actual instances of the class
+//what are instances ? they are the actual objects of the class
+//what are methods ? they are the functions that are part of the class
+//what are attributes ? they are the variables that are part of the class
+//what are constructors ? they are the functions that are called when an object is created
+//what are destructors ? they are the functions that are called when an object is destroyed
+//what are friends ? they are the functions that are part of the class but are not part of the object
+//what are friend functions ? they are the functions that are part of the class but are not part of the object 
+//what are friend classes ? they are the classes that are part of the class but are not part of the object      
+
+int main()
+{
+    cout << "moeez mustafa" << endl;
+}
